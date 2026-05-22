dc75d675db826e6f399c45fb7f754a2ae5cf0213
TontByte <tonycen957@gmail.com>
2026-05-21 22:45:30 -0600

    punto 1, 2, 3, 4, 5 completos

--- Tarea6ED_TonyCen.cpp
+++ Tarea6ED_TonyCen.cpp
@@ -2,18 +2,69 @@
 //
 #include <iostream>
 #include <stdexcept>
+#include <ctime>
+#include "LinkedList.h"
 
 using std::cout;
 using std::runtime_error;
+using std::cin;
 using std::endl;
 
+int getNumValue(int maxIndex, int minIndex) {
+    if (maxIndex < 0 || minIndex > maxIndex) {
+        throw runtime_error("Implementacion erronea de getNumValue");
+    }
 
+    int res;
+    bool gotten = false;
 
-void radixSort(int base) {}
+    while (!gotten) {
+        cin >> res;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Seleccion invalida. Volver a intentar." << endl;
+        }
+        else if (res < 0 || res > maxIndex || res < minIndex) {
+            cin.ignore(1000, '\n');
+            cout << "Seleccion invalida. Volver a intentar." << endl;
+        }
+        else {
+            cin.ignore(1000, '\n');
+            gotten = true;
+            return res;
+        }
+    }
+}
+
+//ask about ammount of digits
+void fillRand(LinkedList<int>& refList, int listSize) {
+    int i = 0;
+    while (i < listSize) {
+        int randNum = rand() % 1000;
+        refList.append(randNum);
+        i++;
+    }
+}
+
+void radixSort(LinkedList<int>& refList, int base) {
+    
+}
 
 
 int main(){
-    std::cout << "Hello World!\n";
+    //ask about this too
+    cout << "Ingrese el tamaño de la lista que desea ordenar (mayor que 1): " << endl;
+    int listSize = getNumValue(2147483647, 1);
+    LinkedList<int>* lista = new LinkedList<int>();
+
+    //ask about this
+    cout << "Ingrese la base numerica que desea utilziar en el ordenamiento (mayor que 2): " << endl;
+    int base = getNumValue(2147483647, 1);
+
+    srand(time(0));
+    fillRand(*lista, listSize);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
